43d105b9bf452b4ea57b064722ea74bba6c4077f
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-09-23 15:45:43 +0200

    step4 done

--- calculator.js
+++ calculator.js
@@ -10,4 +10,14 @@ console.log(calculateBtn);
 
 calculateBtn.addEventListener('click', function () {
   console.log('computing the result...');
+
+  const firstValue = parseInt(firstValueInput.value, 10);
+  const secondValue = parseInt(secondValueInput.value, 10);
+  const operator = operatorInput.value;
+  let result = '';
+
+  console.log('first value : ', firstValue);
+  console.log('second value : ', secondValue);
+  console.log('operator : ', operator);
+  console.log('result', result);
 });
