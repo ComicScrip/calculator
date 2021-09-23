3c3796e0e87650f4cbb78d92e337f22d925987d2
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-09-23 15:06:29 +0200

    step2 done

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const firstValueInput = document.querySelector('#firstValueInput');
+const secondValueInput = document.querySelector('#secondValueInput');
+const operatorInput = document.querySelector('#operator');
+const calculateBtn = document.querySelector('#calculateBtn');
+
+console.log(firstValueInput);
+console.log(secondValueInput);
+console.log(operatorInput);
+console.log(calculateBtn);
